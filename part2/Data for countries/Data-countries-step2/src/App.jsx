1ea1f872dadc80c37a38bbe30857c58c2e53be1d
mustafaalhm <mustafaalhm@gmail.com>
2024-08-18 17:51:44 +0300

    add button next to the name of the country, which when pressed shows the view for that country

--- part2/Data for countries/Data-countries-step2/src/App.jsx
+++ part2/Data for countries/Data-countries-step2/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [countries, setCountries] = useState([]);
 
   const [searchCountry, setSearchCountry] = useState("");
+  const [showCountry, setShowCountry] = useState(null);
 
   const renderLanguage=(lang)=>{
     if(Array.isArray(lang)){
@@ -22,6 +23,7 @@ function App() {
     // hide Message filter if input box is empty
     if(searchCountry.trim() ===""){
       setCountries([]);
+      setShowCountry(null)
       return ;
     }
     const fetchData = async () => {
@@ -30,12 +32,19 @@ function App() {
         const request =await axios.get(url);
         console.log(request.data)
         setCountries(request.data)
+        console.log(request.data)
       } catch (error) {
         console.log("Error in fetching data", error);
       }
     };
     fetchData();
   }, [searchCountry]);
+
+  const handelShowCountery=(country)=>{
+    console.log(country)
+    setShowCountry(country)
+   
+  }
   return (
     <div>
       <h1>Country information </h1>
@@ -58,7 +67,7 @@ function App() {
               {
                 countries.map((country,i)=>{
                   return(
-                    <li key={i}>{country.name.common}</li>
+                    <li key={i}>{country.name.common} <button onClick={()=>handelShowCountery(country)}>show</button></li>
                   )
                 })
               }
@@ -68,15 +77,44 @@ function App() {
       }
       {/* show language and flag if the country shown is one */}
       {
-        countries.length ===1 && (
+        countries.length ===1 ?(<>
+       
           <div>
            <h3>{countries[0].name.common}</h3>
            <p>Capital {countries[0].capital}</p>
            <p>Area {countries[0].area}</p>
            <h3>Languages:</h3>
-           {countries[0].languages && renderLanguage(countries[0].languages)}
+          <p>{countries[0].languages && renderLanguage(countries[0].languages)}</p> 
+            <div className="flag">
+              <p>Flag:</p>
+              <img src={countries[0].flags.png} alt={countries[0].name.common} />
+              <p>Coat OF Arms:</p>
+              <img style={{width:'250px' ,height:'250px'}} src={countries[0].coatOfArms.svg} alt={countries[0].coatOfArms} />
+            </div>
+           
           </div>
-        )
+        
+        </>) :(<>
+        {
+          showCountry && (
+            <div>
+             <h3>{showCountry.name.common}</h3>
+             <p>Capital {showCountry.capital}</p>
+             <p>Area {showCountry.area}</p>
+             <h3>Languages:</h3>
+            <p>{showCountry.languages && renderLanguage(showCountry.languages)}</p> 
+              <div className="flag">
+                <p>Flag:</p>
+                <img src={showCountry.flags.png} alt={showCountry.name.common} />
+                <p>Coat OF Arms:</p>
+                <img style={{width:'250px' ,height:'250px'}} src={showCountry.coatOfArms.svg} alt={showCountry.coatOfArms} />
+              </div>
+             
+            </div>
+          )
+        }
+        
+        </>)
       }
     </div>
   );
